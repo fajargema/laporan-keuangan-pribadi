4f4869bf577bc1b3fbb0996b3b256b830414e7f5
Fajar Gema <fgemar72@gmail.com>
2023-04-18 20:44:24 +0700

    feat(user): add route

--- route/route.go
+++ route/route.go
@@ -1,9 +1,12 @@
 package route
 
 import (
+	"keuangan-pribadi/controllers"
 	m "keuangan-pribadi/middleware"
+	"keuangan-pribadi/utils"
 
 	"github.com/labstack/echo/v4"
+	mid "github.com/labstack/echo/v4/middleware"
 )
 
 func New() *echo.Echo {
@@ -15,6 +18,17 @@ func New() *echo.Echo {
 	}
 	loggerMiddleware := loggerConfig.Init()
 	e.Use(loggerMiddleware)
+	
+	v1 := e.Group("/api/v1")
+	eJwt := v1.Group("")
+	eJwt.Use(mid.JWT([]byte(utils.GetConfig("JWT_SECRET_KEY"))))
+  
+	// Route / to handler function
+	user := controllers.InitUserController()
+	v1.POST("/users/login", user.Login)
+	v1.POST("/users/register", user.Register)
+	eJwt.GET("/users/:email", user.GetByEmail)
+	eJwt.PUT("/users", user.UpdateMe)
 
 	return e
 }
